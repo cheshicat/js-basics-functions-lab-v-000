38069239d30d64a03ccfac0a83a990e12a895011
Allison Howard <alliemhow@gmail.com>
2019-06-05 03:50:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ function distanceFromHqInFeet(block) {
  }
 
  function distanceTravelledInFeet(block1, block2) {
-
+   if (block1 >= block2) {
+     return (block1 - block2) * 264
+   } else {
+     return (block2 - block1) * 264
+   }
  }

7bd59b6c7ce5f03a6565ddb566fc5e920261d6fa
Allison Howard <alliemhow@gmail.com>
2019-06-05 04:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,9 +25,10 @@ function distanceFromHqInFeet(block) {
      return 'cannot travel that far';
    } else {
      if (distance <= 400) {
-       return 0
+       return 0;
+     }
+     else if (distance > 400 && distance <= 2000) {
+        return .02 * (distance - 400);
      }
    }
-
-
  }

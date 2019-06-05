02e45d752a907be237af550ff02f0386bbe2f99c
Allison Howard <alliemhow@gmail.com>
2019-06-05 04:01:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ function distanceFromHqInBlocks(block) {
 }
 
 function distanceFromHqInFeet(block) {
-  return distanceFromHqInBlocks(block) * 264
+  return distanceFromHqInBlocks(block) * 264;
  }
 
  function distanceTravelledInFeet(block1, block2) {
    if (block1 >= block2) {
-     return (block1 - block2) * 264
+     return (block1 - block2) * 264;
    } else {
-     return (block2 - block1) * 264
+     return (block2 - block1) * 264;
    }
+ }
+
+ function calculatesFarePrice (block1, block2) {
+   if distanceTravelledInFeet(block1, block2) < 2500 {
+   } else {
+     return 'cannot travel that far';
+   }
+
  }

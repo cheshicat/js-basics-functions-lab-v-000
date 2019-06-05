eb6a9380af4b73613f3a9ec3b59b3f9df8ef1b16
Allison Howard <alliemhow@gmail.com>
2019-06-05 04:13:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,9 +26,10 @@ function distanceFromHqInFeet(block) {
    } else {
      if (distance <= 400) {
        return 0;
-     }
-     else if (distance > 400 && distance <= 2000) {
+     } else if (distance > 400 && distance <= 2000) {
         return .02 * (distance - 400);
+     } else if (distance > 2000) {
+       return 25;
      }
    }
  }
